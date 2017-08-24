941b066b58929a219f088b0dd817548cd91b4a80
Plehanov Dmitriy <onemanstartup@gmail.com>
2017-08-25 02:58:18 +0300

    faraday

--- lib/mailgun_rails/client.rb
+++ lib/mailgun_rails/client.rb
@@ -1,29 +1,29 @@
 require 'faraday'
 
-
 module MailgunRails
   class Client
     attr_reader :api_key, :domain, :verify_ssl
 
     def initialize(api_key, domain, verify_ssl = true)
-      @api_key = api_key
-      @domain = domain
+      @api_key    = api_key
+      @domain     = domain
       @verify_ssl = verify_ssl
     end
 
     def send_message(options)
-      conn = Faraday.new do |conn|
-        conn.basic_auth('api', api_key)
+      connection = Faraday.new(url: api_url) do |faraday|
+        faraday.request :multipart
+        faraday.adapter  Faraday.default_adapter
       end
-      conn.post(mailgun_url, options)
-    end
 
-    def mailgun_url
-      api_url+"/messages"
+      connection.basic_auth('api', api_key)
+
+      byebug
+      connection.post('messages', options)
     end
 
     def api_url
-      "https://api.mailgun.net/v3/#{domain}"
+      "https://api:#{api_key}@api.mailgun.net/v3/#{domain}"
     end
   end
 end
